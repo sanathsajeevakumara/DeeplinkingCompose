976f29782d433347613a509752ed0ec033121a13
sanathsajeevakumara <sanathsajeevakumara@gmail.com>
2022-12-30 10:41:11 +0530

    create the Main and Detail Screens for Deeplink

--- app/src/main/java/com/example/deeplinkingcompose/MainActivity.kt
+++ app/src/main/java/com/example/deeplinkingcompose/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.deeplinkingcompose
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.example.deeplinkingcompose.ui.theme.DeeplinkingComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +32,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home"
+                    ) {
+                        composable("home") {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Button(onClick = {
+                                    navController.navigate("detail")
+                                }) {
+                                    Text(text = "To Detail Page")
+                                }
+                            }
+                        }
+                        composable(
+                            route ="detail",
+                            deepLinks = listOf(
+                                navDeepLink {
+                                    uriPattern = "https://pl-coding.com/{id}"
+                                    action = Intent.ACTION_VIEW
+                                }
+                            ),
+                            arguments = listOf(
+                                navArgument("id") {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                }
+                            )
+                        ) {
+                            entry -> val id = entry.arguments?.getInt("id")
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(text = "The id is $id")
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DeeplinkingComposeTheme {
-        Greeting("Android")
     }
 }
